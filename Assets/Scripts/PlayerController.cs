f8daee953b87f32ef31afe13a7766f1079424548
FranLenny <42728905+FranLenny@users.noreply.github.com>
2018-10-30 23:46:50 -0400

    next commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,35 +8,57 @@ using UnityEngine;
     {
         public float xMin, xMax, zMin, zMax;
     }
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+    private AudioSource audioSource;
 
-    public class PlayerController : MonoBehaviour
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private float nextFire;
+
+    private Rigidbody rb;
+
+    void Start()
     {
-        public float speed;
-        public float tilt;
-        public Boundary boundary;
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        
+    }
 
-        private Rigidbody rb;
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
 
-        private void Start()
+
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
         {
-            rb = GetComponent<Rigidbody>();
-        }
-
-        void FixedUpdate()
-        {
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
-
-            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
-            rb.velocity = movement * speed;
-
-            rb.position = new Vector3
-            (
-                 Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
-                 0.0f,
-                 Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax));
-
-            rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            audioSource.Play();
         }
     }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = movement * speed;
+
+        rb.position = new Vector3
+        (
+             Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+             0.0f,
+             Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+    }
+}
